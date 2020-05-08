3d255f794e37bedbc71a9b8577e09a22adf4a0cc
rkinnett <rkinnett@gmail.com>
2020-05-08 15:30:04 -0700

    misc cleanup
    Rearranged, removed extraneous includes

--- esp32_fast_plot_demo/esp32_fast_plot_demo.ino
+++ esp32_fast_plot_demo/esp32_fast_plot_demo.ino
@@ -2,11 +2,7 @@
 #include <WebSocketsServer.h>
 #include <WiFi.h>
 #include <ArduinoOTA.h>
-#include <WebAuthentication.h>
-#include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
-#define FS_NO_GLOBALS
-#include <FS.h>
 #include "SPIFFS.h"
 #include <driver/i2s.h>
 #include <driver/adc.h>
@@ -14,9 +10,26 @@
 
 
 //SSID and Password of your WiFi router
-#include "my_wifi.h"  // this contains my ssid and pw; 
-const char* ssid = MY_WIFI_SSID;
-const char* password = MY_WIFI_PW;
+#include "my_wifi.h"  
+///// my_wifi.h contains the following lines with my own wifi info.
+///// You can remove the line above and uncomment the lines below with your info,
+///// or make your own my_wifi.h containing the following lines with your info.
+///// I had to do it this way in order to safely upload to github.
+/*
+const char* ssid = "my_wifi_ssid";
+const char* password = "my_wifi_pw";
+*/
+
+
+/** ADC CONFIG **/
+i2s_port_t i2s_port = I2S_NUM_0;   // I2S Port 
+adc1_channel_t adc_channel = ADC1_CHANNEL_5; //GPIO33  (SET THIS FOR YOUR HARDWARE)
+const uint16_t adc_sample_freq = 44100;
+const uint16_t dma_buffer_len = 1024;
+const uint16_t i2s_buffer_len = dma_buffer_len;
+const uint16_t ws_tx_buffer_len = dma_buffer_len;
+uint16_t* i2s_read_buff = (uint16_t*)calloc(i2s_buffer_len, sizeof(uint16_t));
+uint16_t* ws_send_buffer = (uint16_t*)calloc(ws_tx_buffer_len, sizeof(uint16_t));
 
 
 AsyncWebServer    server(80);
@@ -24,35 +37,50 @@ WebSocketsServer  webSocket = WebSocketsServer(81);
 AsyncEventSource  events("/events");
 AsyncWebServerRequest *request;
 AsyncWebSocket    ws("/test");
+AsyncWebSocketClient * globalClient = NULL;
+void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len);
 
 
-/** CONFIG **/
-i2s_port_t i2s_port = I2S_NUM_0;   // I2S Port 
-adc1_channel_t adc_channel = ADC1_CHANNEL_5; //GPIO33  (SET THIS FOR YOUR HARDWARE)
-const uint16_t adc_sample_freq = 44100;
-const uint16_t dma_buffer_len = 1024;
-const uint16_t i2s_buffer_len = dma_buffer_len;
-const uint16_t ws_tx_buffer_len = dma_buffer_len;
-
-
-TaskHandle_t TaskHandle_2;
 boolean streaming = true;
 boolean sampling  = true;
 unsigned long prev_micros;
-uint16_t* i2s_read_buff = (uint16_t*)calloc(i2s_buffer_len, sizeof(uint16_t));
-uint16_t* ws_send_buffer = (uint16_t*)calloc(ws_tx_buffer_len, sizeof(uint16_t));
 size_t bytes_read;
-
-AsyncWebSocketClient * globalClient = NULL;
+TaskHandle_t samplingTaskHandle;
 
 
 // Prototypes:
 void configure_i2s();
+static void getDataLoopTask(void * pvParameters);
 static const inline void sendSamples();
 static const inline void getSamples();
-void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len);
 
 
+
+//////////////////////////   CONFIGURE I2S SAMPLING   ////////////////////////////////
+void configure_i2s(){
+  i2s_config_t i2s_config = 
+    {
+    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_ADC_BUILT_IN),  // I2S receive mode with ADC
+    .sample_rate = adc_sample_freq,                                               // set I2S ADC sample rate
+    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,                                 // 16 bit I2S (even though ADC is 12 bit)
+    .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,                                 // handle adc data as single channel (right)
+    .communication_format = (i2s_comm_format_t)I2S_COMM_FORMAT_I2S,               // I2S format
+    .intr_alloc_flags = 0,                                                        // 
+    .dma_buf_count = 4,                                                           // number of DMA buffers >=2 for fastness
+    .dma_buf_len = dma_buffer_len,                                                // number of samples per buffer
+    .use_apll = 0,                                                                // no Audio PLL - buggy and not well documented
+  };
+  adc1_config_channel_atten(adc_channel, ADC_ATTEN_11db);
+  adc1_config_width(ADC_WIDTH_12Bit);
+  i2s_driver_install(i2s_port, &i2s_config, 0, NULL);
+ 
+  i2s_set_adc_mode(ADC_UNIT_1, adc_channel);
+  i2s_adc_enable(i2s_port);
+  vTaskDelay(1000);
+}
+
+
+/////////////////////////   SERVER FUNCTIONS   /////////////////////////////////////
 void serverons(){
 
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
@@ -105,11 +133,12 @@ void serverons(){
 }
 
 
-
+//////////////////////////   SETUP   ////////////////////////////////////////
 void setup() {
   Serial.begin(500000);
 
-  //WIFI_Setup();
+
+  // Setup Wifi:
   WiFi.begin(ssid, password);     //Connect to your WiFi router
   Serial.println("");
   // Wait for connection
@@ -123,6 +152,8 @@ void setup() {
   configure_i2s();
   SPIFFS.begin() ? Serial.println("SPIFFS.OK") : Serial.println("SPIFFS.FAIL");
 
+
+  // Setup OTA:
   ArduinoOTA
     .onStart([]() {
       String type;
@@ -148,31 +179,36 @@ void setup() {
       else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
       else if (error == OTA_END_ERROR) Serial.println("End Failed");
     });
-
   ArduinoOTA.begin();
   delay(1000);  // allow time for OTA to break in here
   ArduinoOTA.handle();
   
-  
+
+  // Setup server:
   server.begin();
   serverons();
   ws.onEvent(onWsEvent);
   server.addHandler(&ws);
   webSocket.begin();
-  xTaskCreatePinnedToCore(getDataLoopTask, "getDataLoopTask", 80000, NULL, 0, &TaskHandle_2, 0 );
+  xTaskCreatePinnedToCore(getDataLoopTask, "getDataLoopTask", 80000, NULL, 0, &samplingTaskHandle, 0 );
 
   prev_micros = micros();
 }
 
 
+
+//////////////////////////   MAIN LOOP   /////////////////////////////////
+// Handle infrastructural things in main loop.
+// Sampling is handled in separate tasks outside of this loop.
 void loop() {
-  webSocket.loop();       // WEBSOCKET PACKET LOOP
+  webSocket.loop();
   ArduinoOTA.handle();
   vTaskDelay(1);
 }
 
 
 
+/////////////////////   SAMPLING TASK AND FUNCTIONS  ////////////////////
 static void getDataLoopTask(void * pvParameters){
   for( ;; ){
     //vTaskDelay(1);          // REQUIRED TO RESET THE WATCH DOG TIMER IF WORKFLOW DOES NOT CONTAIN ANY OTHER DELAY
@@ -186,47 +222,12 @@ static void getDataLoopTask(void * pvParameters){
   }
 }
 
-
-void configure_i2s(){
-  i2s_config_t i2s_config = 
-    {
-    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_ADC_BUILT_IN),  // I2S receive mode with ADC
-    .sample_rate = adc_sample_freq,                                               // set I2S ADC sample rate
-    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,                                 // 16 bit I2S (even though ADC is 12 bit)
-    .channel_format = I2S_CHANNEL_FMT_ONLY_RIGHT,                                 // handle adc data as single channel (right)
-    .communication_format = (i2s_comm_format_t)I2S_COMM_FORMAT_I2S,               // I2S format
-    .intr_alloc_flags = 0,                                                        // 
-    .dma_buf_count = 4,                                                           // number of DMA buffers >=2 for fastness
-    .dma_buf_len = dma_buffer_len,                                                // number of samples per buffer
-    .use_apll = 0,                                                                // no Audio PLL - buggy and not well documented
-  };
-  adc1_config_channel_atten(adc_channel, ADC_ATTEN_11db);
-  adc1_config_width(ADC_WIDTH_12Bit);
-  i2s_driver_install(i2s_port, &i2s_config, 0, NULL);
- 
-  i2s_set_adc_mode(ADC_UNIT_1, adc_channel);
-  i2s_adc_enable(i2s_port);
-  vTaskDelay(1000);
-}
-
-
-
-void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
-  Serial.println("Async WebSocket event");
-  if(type == WS_EVT_CONNECT){
-    Serial.println("Async_WebSocket_Connected");
-    globalClient = client;
-  } else if(type == WS_EVT_DISCONNECT){
-    globalClient = NULL;
-    streaming = false;
-    sampling = false;
-    Serial.println("Async_WebSocket_Disconnected");
-  } else {
-    Serial.println("Unhandled Async WebSocket Event");
+static const inline void getSamples(){
+  i2s_read(i2s_port, (void*)i2s_read_buff, i2s_buffer_len*sizeof(uint16_t), &bytes_read, portMAX_DELAY);
+  if(streaming && I2S_EVENT_RX_DONE && bytes_read>0){
+    sendSamples();
   }
 }
-
-
 
 static const inline void sendSamples(){
   // unsigned long micros_now = micros();    // used for troubleshooting sampling rate
@@ -253,9 +254,18 @@ static const inline void sendSamples(){
 
 
 
-static const inline void getSamples(){
-  i2s_read(i2s_port, (void*)i2s_read_buff, i2s_buffer_len*sizeof(uint16_t), &bytes_read, portMAX_DELAY);
-  if(streaming && I2S_EVENT_RX_DONE && bytes_read>0){
-    sendSamples();
+
+void onWsEvent(AsyncWebSocket * server, AsyncWebSocketClient * client, AwsEventType type, void * arg, uint8_t *data, size_t len){
+  Serial.println("Async WebSocket event");
+  if(type == WS_EVT_CONNECT){
+    Serial.println("Async_WebSocket_Connected");
+    globalClient = client;
+  } else if(type == WS_EVT_DISCONNECT){
+    globalClient = NULL;
+    streaming = false;
+    sampling = false;
+    Serial.println("Async_WebSocket_Disconnected");
+  } else {
+    Serial.println("Unhandled Async WebSocket Event");
   }
 }
